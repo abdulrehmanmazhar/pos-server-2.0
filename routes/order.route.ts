ea7ef8c629e41c227ceffdc238e9056f5c385a05
abdulrehmanmazhar <abdulrehmanmazhar02@gmail.com>
2025-02-08 20:57:35 +0500

    order udhar and paymen routest done

--- routes/order.route.ts
+++ routes/order.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { addOrder, createCart, deleteCart, deleteOrder, getAllOrders, getOrder } from "../controllers/order.controller";
+import { addOrder, addOrderPayment, createCart, deleteCart, deleteOrder, getAllOrders, getOrder, returnOrderUdhar } from "../controllers/order.controller";
 import { PDFgenerator } from "../utils/puppeteer";
 const router = express.Router();
 
@@ -8,6 +8,8 @@ router.post("/fill-cart/:id",isAuthenticated, createCart);
 // router.put("/edit-cart/:id/:index",isAuthenticated,  editCart);
 router.delete("/delete-cart/:id/:index",isAuthenticated,  deleteCart);
 router.post("/add-order/:id",isAuthenticated,  addOrder);
+router.post("/add-order-payment/:id",isAuthenticated,  addOrderPayment);
+router.post("/return-order-udhar/:id",isAuthenticated,  returnOrderUdhar);
 router.delete('/delete-order/:id',isAuthenticated, authorizeRoles("admin"), deleteOrder)
 router.get("/get-order/:id",isAuthenticated,getOrder);
 router.get("/get-orders",isAuthenticated, getAllOrders);
