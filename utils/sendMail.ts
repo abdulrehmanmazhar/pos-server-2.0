5200cceda764482b160bdbaa97bd1555a0b22a8b
abdulrehmanmazhar <abdulrehmanmazhar02@gmail.com>
2025-02-19 22:20:54 +0500

    user registration fixed

--- utils/sendMail.ts
+++ utils/sendMail.ts
@@ -22,7 +22,7 @@ const sendMail = async (options:EmailOptions): Promise<void> =>{
     const {email, subject, template, data} = options;
     
     const templatePath = path.join(__dirname,"../mails", template)
-    // console.log(data);
+    console.log(data);
 
     let html:string = await ejs.renderFile(templatePath, data)
     const mailOptions = {
